9c947d506c5efe9abb5621c350ea73362ccb08df
Kyrylo <krllmsk@gmail.com>
2021-04-28 09:25:16 +0300

    fixed change theme

--- src/index.js
+++ src/index.js
@@ -13,31 +13,34 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-setDefaultTheme();
+setTheme();
 
 const menuCardsMarkup = createMenuCardsMarkup(menu);
-
 refs.menuList.insertAdjacentHTML('beforeend', menuCardsMarkup);
 
 refs.themeSwitcher.addEventListener('change', changeTheme);
 
 function changeTheme() {
-  let currentTheme = localStorage.getItem('theme');
-  if (currentTheme === Theme.LIGHT) {
+  let markupTheme = refs.body.classList.value;
+
+  if (markupTheme === Theme.LIGHT) {
     refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
     localStorage.setItem('theme', Theme.DARK);
-  } else {
+    return
+  }
+
+  if (markupTheme === Theme.DARK){
     refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
     localStorage.setItem('theme', Theme.LIGHT);
   }
 }
 
-function setDefaultTheme() {
-  let defaultTheme = refs.body.classList.value;
+function setTheme() {
+  let markupTheme = refs.body.classList.value;
   let localStorageTheme = localStorage.getItem('theme');
 
-  if (defaultTheme !== localStorageTheme) {
-    refs.body.classList.replace(defaultTheme, localStorageTheme);
+  if (localStorageTheme && markupTheme !== localStorageTheme) {
+    refs.body.classList.replace(markupTheme, localStorageTheme);
   }
 
   if (localStorageTheme === Theme.DARK) {
